4f0b57cfdb6b91d21d20c6c9b0c1039d974a4496
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2015-07-24 07:34:15 -0300

    change conjure approach

--- client.go
+++ client.go
@@ -1,6 +1,17 @@
 package conjure
 
-import "github.com/fsouza/go-dockerclient"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+type Entity struct {
+	Name       string             `json:"Name"`
+	Config     *docker.Config     `json:"Config"`
+	HostConfig *docker.HostConfig `json:"HostConfig"`
+}
 
 type Client struct {
 	*docker.Client
@@ -22,4 +33,46 @@ func NewClient() (*Client, error) {
 	client = Client{dockerClient}
 
 	return &client, err
+}
+
+func (c *Client) Run(containerSpec string) (*docker.Container, error) {
+	container := Entity{}
+
+	err := json.Unmarshal([]byte(containerSpec), &container)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Spec: %+v\n", container)
+
+	opts := docker.CreateContainerOptions{
+		Name:       container.Name,
+		Config:     container.Config,
+		HostConfig: container.HostConfig,
+	}
+
+	dockerCtn, err := c.CreateContainer(opts)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	err = c.StartContainer(dockerCtn.ID, nil)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return dockerCtn, nil
+}
+
+func (c *Client) Remove(id string) error {
+	rmCtnOpt := docker.RemoveContainerOptions{
+		ID:            id,
+		RemoveVolumes: true,
+		Force:         true,
+	}
+
+	return c.RemoveContainer(rmCtnOpt)
 }
